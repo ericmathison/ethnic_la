bc423ad14ad3227300ae7f542f89ef24c2441898
Eric <erics.secondary.email@gmail.com>
2018-03-04 01:40:14 -0800

    Tweak script to import data into new schema

--- lib/scripts/import_ethnic_church_data.rb
+++ lib/scripts/import_ethnic_church_data.rb
@@ -11,5 +11,20 @@ CSV.foreach(ARGV[0], headers: headers) do |csv|
     end
   end
 
-  EthnicChurch.create(hash)
+  ethnic_church = hash.slice(:name, :phone, :website, :pastors_name)
+  language = hash.slice(:language)
+  country = hash.slice(:country)
+  religious_background = hash.slice(:religious_background)
+  notes = hash.slice(:notes)
+  address = hash.slice(:street, :city, :state, :zip)
+
+  EthnicChurch.transaction do
+    ethnic_church = EthnicChurch.new(ethnic_church)
+    ethnic_church.build_language({name: language[:language]})
+    ethnic_church.build_country({name: country[:country]})
+    ethnic_church.build_religious_background({persuasion: religious_background[:religious_background]})
+    ethnic_church.build_note({content: notes[:notes]})
+    ethnic_church.build_address(address)
+    ethnic_church.save!
+  end
 end
