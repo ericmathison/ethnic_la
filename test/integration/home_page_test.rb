f60a3baa99d1617c4423b8928fafa97e15f8be97
Eric <erics.secondary.email@gmail.com>
2018-03-10 23:21:07 -0800

    Name language table fields (using class)
    * This makes the test less dependent on the specific tag name used

--- test/integration/home_page_test.rb
+++ test/integration/home_page_test.rb
@@ -21,7 +21,7 @@ class HomePageTest < ActionDispatch::IntegrationTest
   test 'language page lists ethnic churches of given language and no others' do
     get language_path(languages(:chinese).id)
 
-    assert_select 'td', languages(:chinese).name
-    assert_select 'td', {count: 0, text: languages(:english).name}
+    assert_select '.language-name', languages(:chinese).name
+    assert_select '.language-name', {count: 0, text: languages(:english).name}
   end
 end
