9ceafe04ca46ac40ad5bd796b6063304cbf0852c
Eric <erics.secondary.email@gmail.com>
2018-03-12 19:50:20 -0700

    Use correct variable name (country, not lang)

--- test/models/country_test.rb
+++ test/models/country_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class CountryTest < ActiveSupport::TestCase
   test 'name attribute should be unique' do
     # using an existing fixture name
-    lang = Country.new(name: countries(:china).name)
-    refute lang.valid?
+    country = Country.new(name: countries(:china).name)
+    refute country.valid?
   end
 end
