b7fdb2c2f11f779bc92284bd3375ecc604ef80a1
Eric <erics.secondary.email@gmail.com>
2018-03-20 23:56:19 -0700

    Display name of language on language show page

--- app/controllers/languages_controller.rb
+++ app/controllers/languages_controller.rb
@@ -1,7 +1,7 @@
 class LanguagesController < ApplicationController
   def show
-    language = Language.where(id: params[:id]).first
-    @ethnic_churches = EthnicChurch.joins(:language).where(Language.arel_table[:id].eq(language.id))
+    @language = Language.where(id: params[:id]).first
+    @ethnic_churches = EthnicChurch.joins(:language).where(Language.arel_table[:id].eq(@language.id))
     @languages = Language.list
   end
 end
