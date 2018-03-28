4f7ea4d04c5e81bd53beb8b55d31445a81d837a7
Eric <erics.secondary.email@gmail.com>
2018-03-28 02:05:46 -0700

    Use %i syntax for array of symbols

--- lib/scripts/import_ethnic_church_data.rb
+++ lib/scripts/import_ethnic_church_data.rb
@@ -1,6 +1,6 @@
 require 'csv'
 
-headers = [:street, :city, :country, :website, :language, :name, :notes, :pastors_name, :phone, :religious_background, :state, :zip]
+headers = %i[street city country website language name notes pastors_name phone religious_background state zip]
 
 CSV.foreach(ARGV[0], headers: headers) do |csv|
   hash = csv.to_h.transform_values(&:strip).transform_values do |value|
