4dbc3910059944d8e22973c22d4cbbbdba4a515b
Eric <erics.secondary.email@gmail.com>
2018-04-01 14:53:31 -0700

    Remove dependency on @language_array instance var

--- app/controllers/ethnic_churches_controller.rb
+++ app/controllers/ethnic_churches_controller.rb
@@ -10,7 +10,6 @@ class EthnicChurchesController < ApplicationController
 
   def new
     @ethnic_church = EthnicChurch.new
-    @language_array = Language.pluck(:name).reject(&:nil?).sort.map { |lang| [lang, lang] }
   end
 
   def create
