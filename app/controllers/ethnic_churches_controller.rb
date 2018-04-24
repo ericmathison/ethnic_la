ca199a599c2ad7069e2b248f0e55619bc349de5a
Eric <erics.secondary.email@gmail.com>
2018-04-23 23:16:55 -0700

    Inject dependencies into EthnicChurchBuilder

--- app/controllers/ethnic_churches_controller.rb
+++ app/controllers/ethnic_churches_controller.rb
@@ -13,7 +13,14 @@ class EthnicChurchesController < ApplicationController
   end
 
   def create
-    @ethnic_church = EthnicChurchBuilder.instantiate(params)
+    @ethnic_church = EthnicChurchBuilder.instantiate(
+      ethnic_church: ethnic_church,
+      languages: languages,
+      country: country,
+      religious_background: religious_background,
+      address: address,
+      note: note
+    )
 
     if @ethnic_church.save
       redirect_to ethnic_church_path(@ethnic_church), notice: 'Successfully added new Ethnic Church'
@@ -21,5 +28,49 @@ class EthnicChurchesController < ApplicationController
       flash.now.alert = @ethnic_church.errors.full_messages.join(', ')
       render 'new'
     end
+  end
+
+  private
+
+  def ethnic_church
+    EthnicChurch.new(ethnic_church_params)
+  end
+
+  def languages
+    params[:language][:name].reject(&:blank?).map do |lang_name|
+      Language.find_or_initialize_by(name: lang_name)
+    end
+  end
+
+  def country
+    country = params[:country][:name]
+    Country.find_or_initialize_by(name: country)
+  end
+
+  def address
+    Address.new(address_params)
+  end
+
+  def note
+    Note.new(note_params)
+  end
+
+  def note_params
+    params.require(:note).permit(:content)
+  end
+
+  def religious_background
+    persuasion = params[:religious_background][:persuasion]
+    ReligiousBackground.find_or_initialize_by(persuasion: persuasion)
+  end
+
+  def ethnic_church_params
+    params.require(:ethnic_church)
+      .permit(:name, :phone, :website, :pastors_name, :email)
+      .transform_values { |val| val if val.present? }
+  end
+
+  def address_params
+    params.require(:address).permit(:street, :city, :zip)
   end
 end
--- test/models/ethnic_church_builder_test.rb
+++ test/models/ethnic_church_builder_test.rb
@@ -1,54 +1,41 @@
 require 'test_helper'
 
 class EthnicChurchBuilderTest < ActiveSupport::TestCase
-  test 'EthnicChurchBuilder.instantiate' do
-    params = ActionController::Parameters.new({
-      ethnic_church: {
-        name: 'Foobar Korean Church',
-        phone: '123-456-7890',
-        website: 'https://example.com',
-        pastors_name: 'Peter Kim',
-        email: 'contact@example.com'
-      },
-      language: {
-        name: ['Korean', 'English']
-      },
-      country: {
-        name: 'Korea'
-      },
-      religious_background: {
-        persuasion: 'Protestant'
-      },
-      address: {
-        street: '1234 Foo St.',
-        city: 'Los Angeles',
-        zip: '90000'
-      },
-      note: {
-        content: 'Look! A note!'
-      }
-    })
+  test 'EthnicChurchBuilder.instantiate assigns associated objects' do
+    ethnic_church = ethnic_churches(:foobar)
+    languages = [languages(:chinese), languages(:english)]
+    country = countries(:china)
+    religious_background = religious_backgrounds(:protestant)
+    address = addresses(:los_angeles)
+    note = notes(:one)
 
-    ec = EthnicChurchBuilder.instantiate(params)
+    ec = EthnicChurchBuilder.instantiate(
+      ethnic_church: ethnic_church,
+      languages: languages,
+      country: country,
+      religious_background: religious_background,
+      address: address,
+      note: note
+    )
 
-    assert_equal ec.name, params[:ethnic_church][:name]
-    assert_equal ec.phone, params[:ethnic_church][:phone]
-    assert_equal ec.website, params[:ethnic_church][:website]
-    assert_equal ec.pastors_name, params[:ethnic_church][:pastors_name]
-    assert_equal ec.email, params[:ethnic_church][:email]
+    assert_equal ec.name, ethnic_church.name
+    assert_equal ec.phone, ethnic_church.phone
+    assert_equal ec.website, ethnic_church.website
+    assert_equal ec.pastors_name, ethnic_church.pastors_name
+    assert_equal ec.email, ethnic_church.email
 
-    params[:language][:name].each do |lang_name|
-      assert_includes ec.languages.map(&:name), lang_name
+    languages.each do |lang|
+      assert_includes ec.languages.map(&:name), lang.name
     end
 
-    assert_equal ec.country.name, params[:country][:name]
+    assert_equal ec.country.name, country.name
 
-    assert_equal ec.religious_background.persuasion, params[:religious_background][:persuasion]
+    assert_equal ec.religious_background.persuasion, religious_background.persuasion
 
-    assert_equal ec.address.street, params[:address][:street]
-    assert_equal ec.address.city, params[:address][:city]
-    assert_equal ec.address.zip, params[:address][:zip]
+    assert_equal ec.address.street, address.street
+    assert_equal ec.address.city, address.city
+    assert_equal ec.address.zip, address.zip
 
-    assert_equal ec.note.content, params[:note][:content]
+    assert_equal ec.note.content, note.content
   end
 end
