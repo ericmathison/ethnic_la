20ccdf2390ddd70273e6efe2a55d39c705125ff7
Eric Mathison <dev@ericmathison.io>
2018-05-17 01:03:38 -0700

    Include 'CA' in full_address and gracefully handle missing info

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,6 +2,8 @@ class Address < ApplicationRecord
   belongs_to :ethnic_church
 
   def full_address
-    "#{street}\n#{city}, #{state} #{zip}"
+    return "#{street}\n#{city}, CA #{zip}" if street && city && zip
+    return "#{city}, CA" if city
+    'CA'
   end
 end
--- test/models/address_test.rb
+++ test/models/address_test.rb
@@ -1,10 +1,26 @@
 require 'test_helper'
 
 class AddressTest < ActiveSupport::TestCase
-  test 'Address#full_address' do
+  test 'Address#full_address returns formatted address' do
     addr = addresses(:los_angeles)
-    expected_address =  "#{addr.street}\n#{addr.city}, #{addr.state} #{addr.zip}"
+    expected_address =  "#{addr.street}\n#{addr.city}, CA #{addr.zip}"
 
-    assert_equal addr.full_address, expected_address
+    assert_equal expected_address, addr.full_address
+  end
+
+  test 'Address#full_address returns "CA" when all fields are nil' do
+    addr = Address.new
+    expected_address =  'CA'
+
+    assert_equal expected_address, addr.full_address
+  end
+
+  test 'Address#full_address returns "city, CA" when only city is present' do
+    addr = Address.new
+    city = 'Footown'
+    addr.city = city
+    expected_address = "#{city}, CA"
+
+    assert_equal expected_address, addr.full_address
   end
 end
