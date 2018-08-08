583534d194b347733bb754dbe3eda34755caeca4
Eric Mathison <dev@ericmathison.io>
2018-08-08 15:04:54 -0700

    Remove Language.list method
    It is no longer being used anywhere.

--- test/models/language_test.rb
+++ test/models/language_test.rb
@@ -1,25 +1,6 @@
 require 'test_helper'
 
 class LanguageTest < ActiveSupport::TestCase
-  test 'every language in Language.list has an id and a name' do
-    assert Language.list.first.has_key?(:id)
-    assert Language.list.first.has_key?(:name)
-  end
-
-  test 'Language.list has every stored language' do
-    assert_equal Language.list.length, Language.count
-  end
-
-  test 'name value matches name of corresponding Language record' do
-    l = Language.first
-    expected_name = l.name
-    actual_name = Language.list
-                    .select { |languages| languages[:id] == l.id }
-                    .first[:name]
-
-    assert_equal expected_name, actual_name
-  end
-
   test 'name attribute should be unique' do
     # using an existing fixture name
     lang = Language.new(name: languages(:chinese).name)
