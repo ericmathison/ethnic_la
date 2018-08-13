4f62e4e6a763d098b8ea91bc69ca9623850668b6
Eric Mathison <dev@ericmathison.io>
2018-08-13 11:47:26 -0700

    WIP

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,4 +7,16 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-console.log('Hello World from Webpacker')
+import HeatmapJS from 'heatmap.js'
+
+window.onload = function() {
+    let h = HeatmapJS.create({
+      container: document.querySelector('.heatmap_area')
+    });
+
+    h.setData({
+      max: 10,
+      min: 0,
+      data: [{ x: 10, y: 15, value: 5}, { x: 370, y: 70, value: 4}, { x: 330, y: 70, value: 2}, { x: 130, y: 70, value: 9}]
+    });
+}
