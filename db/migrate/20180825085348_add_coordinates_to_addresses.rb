0693bf0bbd86d87b975422b83360446b479a0fb2
Eric Mathison <dev@ericmathison.io>
2018-08-27 11:34:03 -0700

    Add latitude and longitude to Address

---
+++ db/migrate/20180825085348_add_coordinates_to_addresses.rb
@@ -0,0 +1,6 @@
+class AddCoordinatesToAddresses < ActiveRecord::Migration[5.2]
+  def change
+    add_column :addresses, :latitude, :float
+    add_column :addresses, :longitude, :float
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_08_223842) do
+ActiveRecord::Schema.define(version: 2018_08_25_085348) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2018_08_08_223842) do
     t.bigint "ethnic_church_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["ethnic_church_id"], name: "index_addresses_on_ethnic_church_id"
   end
 
